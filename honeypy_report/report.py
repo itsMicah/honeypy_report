f5c89288a09f31f3ba32a3ed717bc6e2776fc68e
Micah Prescott <mprescott@Lexannes-MacBook-Pro.local>
2017-11-01 06:00:08 -0400

    Add configs to report service

--- honeypy_report/report.py
+++ honeypy_report/report.py
@@ -7,9 +7,12 @@ from os.path import dirname, abspath
 
 from honeypy_report.controller import ReportController
 
-reportController = ReportController()
 reportApi = Flask(__name__)
 CORS(reportApi, resources={r'\/report\/?.*': {'origins': 'http://localhost:4200'}})
+
+reportApi.config.from_object('honeypy_report.configs.development')
+reportApi.config.from_envvar('honeypyReportEnvironment', silent = True)
+reportController = ReportController(config = reportApi.config)
 
 def createResponse(data):
     response = {
@@ -46,19 +49,6 @@ def patchReport(reportId):
     results = ReportController(reportId).patchReport(data)
     return createResponse(results)
 
-# @reportApi.route("/report/<reportId>", methods = ["PUT"])
-# def putReport(reportId):
-#     try:
-#         data = request.get_json()
-#         if "_id" in data:
-#             data.pop("_id")
-#         elif "id" in data:
-#             data.pop("id")
-#         data = ReportController(reportId).putReport(data)
-#         return createResponse(data, "success", 200)
-#     except:
-#         return createResponse(None, "failure", 500)
-
 @reportApi.route("/report/<reportId>/add", methods = ["PATCH"])
 def addToReport(reportId):
     data = request.get_json()
@@ -78,7 +68,7 @@ def deleteReport(reportId):
 
 
 def main():
-    reportApi.run(host="0.0.0.0", port=30002, threaded=True)
+    reportApi.run(host=reportApi.config["IP"], port=reportApi.config["PORT"], threaded=True)
 
 # Run service
 if __name__ == "__main__":
