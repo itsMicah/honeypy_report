9f9ab67f3586b355a81137c6b96c2d9554f8bb04
Micah Prescott <mprescott@codesigned.com>
2018-05-03 15:40:59 -0400

    Update configs with new environment service

--- honeypy_report/configs/development.py
+++ honeypy_report/configs/development.py
@@ -9,25 +9,36 @@ PORT=80
 TEST_IP="127.0.0.1"
 TEST_PORT=30001
 TEST_URL="http://" + TEST_IP + ":" + str(TEST_PORT)
-TEST_DB="test"
+TEST_DB_NAME="test"
+TEST_DB_COLLECTION="test"
 
 # Report Service Configs
 REPORT_IP="127.0.0.1"
 REPORT_PORT=30002
 REPORT_URL="http://" + REPORT_IP + ":" + str(REPORT_PORT)
-REPORT_DB="report"
+REPORT_DB_NAME="report"
+REPORT_DB_COLLECTION="report"
 
 # Host Service Configs
 HOST_IP="127.0.0.1"
 HOST_PORT=30003
 HOST_URL="http://" + HOST_IP + ":" + str(HOST_PORT)
-HOST_DB="host"
+HOST_DB_NAME="host"
+HOST_DB_COLLECTION="host"
 
 # Set Service Configs
 SET_IP="127.0.0.1"
 SET_PORT=30004
 SET_URL="http://" + SET_IP + ":" + str(SET_PORT)
-SET_DB="set"
+SET_DB_NAME="set"
+SET_DB_COLLECTION="set"
+
+# Environment Service Configs
+ENVIRONMENT_IP="127.0.0.1"
+ENVIRONMENT_PORT=30005
+ENVIRONMENT_URL="http://" + ENVIRONMENT_IP + ":" + str(ENVIRONMENT_PORT)
+ENVIRONMENT_DB_NAME="environment"
+ENVIRONMENT_DB_COLLECTION="environment"
 
 # Database Configs
 DATABASE_IP="127.0.0.1"
--- honeypy_report/configs/production.py
+++ honeypy_report/configs/production.py
@@ -7,35 +7,46 @@ IP="0.0.0.0"
 PORT=80
 
 # Test Service Configs
-TEST_IP="test-service.default.svc.cluster.local"
+TEST_IP="test-service"
 TEST_PORT=80
 TEST_URL="http://" + TEST_IP
-TEST_DB="test"
+TEST_DB_NAME="test"
+TEST_DB_COLLECTION="test"
 
 # Report Service Configs
-REPORT_IP="report-service.default.svc.cluster.local"
+REPORT_IP="report-service"
 REPORT_PORT=80
 REPORT_URL="http://" + REPORT_IP
-REPORT_DB="report"
+REPORT_DB_NAME="report"
+REPORT_DB_COLLECTION="report"
 
 # Host Service Configs
-HOST_IP="host-service.default.svc.cluster.local"
+HOST_IP="host-service"
 HOST_PORT=80
 HOST_URL="http://" + HOST_IP
-HOST_DB="host"
+HOST_DB_NAME="host"
+HOST_DB_COLLECTION="host"
 
 # Set Service Configs
-SET_IP="set-service.default.svc.cluster.local"
+SET_IP="set-service"
 SET_PORT=80
 SET_URL="http://" + SET_IP
-SET_DB="set"
+SET_DB_NAME="set"
+SET_DB_COLLECTION="set"
+
+# Environment Service Configs
+ENVIRONMENT_IP="environment-service"
+ENVIRONMENT_PORT=80
+ENVIRONMENT_URL="http://" + ENVIRONMENT_IP + ":" + str(ENVIRONMENT_PORT)
+ENVIRONMENT_DB_NAME="environment"
+ENVIRONMENT_DB_COLLECTION="environment"
 
 # Database Configs
-DATABASE_IP="honeypy-db-mongodb.default.svc.cluster.local"
+DATABASE_IP="honeypy-db-mongodb"
 DATABASE_PORT=27017
 
 # Redis Configs
-REDIS_HOST="honeypy-rds-redis.default.svc.cluster.local"
+REDIS_HOST="honeypy-rds-redis"
 REDIS_PORT=6379
 REDIS_PASSWORD="P@r41LaX?!"
 
